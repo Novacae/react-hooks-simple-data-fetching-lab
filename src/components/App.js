91d69bf32c94a8bcf2da3b936f63670c2fccaab2
aman githinji <amangithinji09@gmail.com>
2023-09-15 18:04:08 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,37 @@
 // create your App component here
+import react, { useEffect, useState } from "react";
+
+function App() {
+  const Api = `https://dog.ceo/api/breeds/image/random`;
+  const [isloading, setIskoading] = useState(true);
+  const [dogs, setDogs] = useState(``);
+
+  useEffect(() => {
+    const getDogs = async () => {
+      try {
+        const res = await fetch(Api);
+        const resp = await res.json();
+        setDogs(resp);
+        setIskoading(false);
+      } catch (error) {
+        console.log("error dawg", error);
+        setIskoading(false);
+      }
+    };
+    getDogs();
+  }, []);
+
+  return (
+    <div>
+      {isloading ? (
+        <p>Loading...</p>
+      ) : (
+        <img
+          src={"https://images.dog.ceo/breeds/bulldog-english/mami.jpg"}
+          alt="A Random Dog"
+        />
+      )}
+    </div>
+  );
+}
+export default App;
